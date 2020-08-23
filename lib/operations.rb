b910e29106a252927c4c92a9a713837c32fe36a6
Reynelle T <rtalford13@gmail.com>
2020-08-23 19:05:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
+   if speed>60 speed<40
+          "unsafe"
+   else 
+           "safe"
+   end
 
-end
 
 
 
 def not_safe?(speed)
-	
+     speed<40 ? "not safe" : "safe"	
+     speed>60 ? "not safe" : "safe"
+    end	
 end
-	
-
 
